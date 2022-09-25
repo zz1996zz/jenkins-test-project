35ecbdf1ad164cfac006585c5398eb66a5d36771
JeongSu <zz1996zz@naver.com>
2022-09-25 15:07:16 +0900

    create spring boot project

---
+++ src/main/java/test/jenkinstestproject/JenkinsTestProjectApplication.java
@@ -0,0 +1,13 @@
+package test.jenkinstestproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsTestProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsTestProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/test/jenkinstestproject/JenkinsTestProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package test.jenkinstestproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+class JenkinsTestProjectApplicationTests {
+
+//    @Test
+    void contextLoads() {
+    }
+
+}
